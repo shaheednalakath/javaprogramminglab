459b9e051cd0c015c46e049cf04df54f44e8a62c
MUHAMMED ABDUL SHAHEED.N <116936159+shaheednalakath@users.noreply.github.com>
2023-06-11 15:59:27 +0530

    Update sort.java

--- javapromca34labcycle2/sort.java
+++ javapromca34labcycle2/sort.java
@@ -1,23 +1,24 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class sort{
+class sortstring{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of members:");
-        int n = sc.nextInt();
-        String[] names = new String[n];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter string");
+        String str=sc.next();
+        char[] charArray=str.toCharArray();
+        int n=str.length();
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(charArray[j]>charArray[j+1])
+                {
+                    char temp=charArray[j];
+                    charArray[j]=charArray[j+1];
+                    charArray[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(charArray);
 
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter name:");
-            names[i] = sc.next();
         }
 
-        System.out.println("Names before sorting: " + Arrays.toString(names));
-
-        Arrays.sort(names);
-
-        System.out.println("Names after sorting: " + Arrays.toString(names));
     }
-}
-
