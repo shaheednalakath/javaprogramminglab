7e82757663cfd80203e31c992364f2e5985f58f7
MUHAMMED ABDUL SHAHEED.N <116936159+shaheednalakath@users.noreply.github.com>
2023-06-23 11:06:49 +0530

    Update Circle.java

--- javalablabcycle3/Circle.java
+++ javalablabcycle3/Circle.java
@@ -5,12 +5,12 @@ public class Circle {
         float area = (float) (Math.PI * radius * radius);
         System.out.println("Area of the circle: " + area);
     }
-    
+
     void calculateArea(double side) {
         double area = side * side;
         System.out.println("Area of the square: " + area);
     }
-    
+
     void calculateArea(float length, float width) {
         float area = length * width;
         System.out.println("Area of the rectangle: " + area);
@@ -18,9 +18,38 @@ public class Circle {
 
     public static void main(String[] args) {
         Circle c1 = new Circle();
-        c1.calculateArea(3.0f);
-        c1.calculateArea(3.0d);
-        c1.calculateArea(2.0f, 5.0f);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select an option:");
+        System.out.println("1. Calculate the area of a circle");
+        System.out.println("2. Calculate the area of a square");
+        System.out.println("3. Calculate the area of a rectangle");
+
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.print("Enter the radius of the circle: ");
+                float radius = scanner.nextFloat();
+                c1.calculateArea(radius);
+                break;
+            case 2:
+                System.out.print("Enter the side length of the square: ");
+                double side = scanner.nextDouble();
+                c1.calculateArea(side);
+                break;
+            case 3:
+                System.out.print("Enter the length of the rectangle: ");
+                float length = scanner.nextFloat();
+                System.out.print("Enter the width of the rectangle: ");
+                float width = scanner.nextFloat();
+                c1.calculateArea(length, width);
+                break;
+            default:
+                System.out.println("Invalid option");
+                break;
+        }
+
+        scanner.close();
     }
 }
-
